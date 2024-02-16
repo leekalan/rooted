0a31199d3f59228da36393d279489b39d5237c58
Flamearoo <kalanlee7258@gmail.com>
2024-02-16 16:21:51 +1100

    Update

--- src/change_directory.rs
+++ src/change_directory.rs
@@ -2,17 +2,15 @@ use roped::*;
 
 use crate::State;
 
-#[allow(dead_code)]
 #[derive(Debug)]
-pub struct ChangeDirectory {
-    path: String,
-}
+pub struct ChangeDirectory;
 
 impl Strand for ChangeDirectory {
     type State = State;
 
-    fn run(_: &mut Self::State, input: &str, _: &[char]) -> Result<(), String> {
-        let new_dir = crate::offset_dir(&std::path::PathBuf::from(input))?;
+    fn run(_: &mut Self::State, input: &str, ws: &[char]) -> Result<(), String> {
+        let path = input.trim_start_matches(ws).trim_end_matches(ws).replace('~', "..");
+        let new_dir = crate::offset_dir(&std::path::PathBuf::from(path))?;
         crate::set_dir(&new_dir)
     }
 }
