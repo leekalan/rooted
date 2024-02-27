51f29cdb43088fe285462d4c6bf708eda48873ee
kalan lee <kalanjlee@gmail.com>
2024-02-27 14:50:15 +1100

    Trying something else

--- src/paste.rs
+++ src/paste.rs
@@ -1,3 +1,6 @@
+mod functions;
+use functions::*;
+
 use crate::*;
 
 use std::{fs::{self, File}, path::Path};
@@ -32,83 +35,17 @@ impl Strand for PasteDirectory {
             state::Moving::Move(p, m) => (p, m),
         };
 
-        println!("{}", paste_dir(&copy_dir, &destination_dir, move_type, move_info)?);
+        println!("{}", paste_dir(&copy_dir, &destination_dir, move_type, &move_info)?);
 
         Ok(())
     }
 }
-#[derive(PartialEq, Eq)]
-enum PathType {
-    Folder,
-    File,
+
+fn paste_dir(origin: &Path, destination: &Path, move_type: &MoveType, move_info: &MoveInfo) -> Result<String, String> {
+    todo!()
 }
 
 enum MoveInfo {
     Ident,
     Contents,
-}
-
-fn paste_dir(origin: &Path, destination: &Path, move_type: &MoveType, move_info: MoveInfo) -> Result<String, String> {
-    let origin_type = if let Ok(meta) = origin.metadata() {
-        if meta.is_file() {
-            PathType::File
-        } else if meta.is_dir() {
-            PathType::Folder
-        } else {
-            return Err("Origin is not copyable".to_string())
-        }
-    } else {
-        return Err("Origin does not exist".to_string())
-    };
-
-    if destination.exists() {
-        let destination_type = if let Ok(meta) = destination.metadata() {
-            if meta.is_file() {
-                PathType::File
-            } else if meta.is_dir() {
-                PathType::Folder
-            } else {
-                return Err("Destination cannot be pasted too".to_string())
-            }
-        } else {
-            return Err("Destination is corrupted".to_string())
-        };
-
-        if origin_type != destination_type { return Err("Origin and Destination do not match".to_string()) }
-
-        println!("Destination already exists. Are you sure you want to add contents? (y/_)");
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).map_err(|_| "Cancelling..".to_string())?;
-        if input.trim().to_lowercase() != "y" {
-            return Ok("Cancelling..".to_string())
-        }
-
-        if origin_type == PathType::File {
-            return copy_file_contents(origin, destination, move_type)
-        }
-    } else {
-        match origin_type {
-            PathType::Folder => fs::create_dir_all(origin).map_err(|_| "Could not create the specified destination".to_string())?,
-            PathType::File => {
-                return copy_file_contents(origin, destination, move_type)
-            },
-        }
-    }
-
-    match move_info {
-        MoveInfo::Ident => todo!(),
-        MoveInfo::Contents => todo!(),
-    }
-}
-
-fn copy_file_contents(origin: &Path, destination: &Path, move_type: &MoveType) -> Result<String, String> {
-    //Check if exist
-
-    //let file = std::fs::File::create(origin).map_err(|_| "Could not create the specified destination".to_string())?;
-    
-    todo!()
-}
-
-fn paste_dir_contents(origin: &Path, destination: &Path, move_type: &MoveType) -> Result<String, String> {
-    todo!()
 }
---
+++ src/paste/functions.rs
@@ -0,0 +1,33 @@
+use std::path::Path;
+
+fn copy_file_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    todo!()
+}
+
+fn copy_file_contents_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    todo!()
+}
+
+fn copy_folder_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    todo!()
+}
+
+fn copy_folder_contents_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    todo!()
+}
+
+fn cut_file_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    todo!()
+}
+
+fn cut_file_contents_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    todo!()
+}
+
+fn cut_folder_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    todo!()
+}
+
+fn cut_folder_contents_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    todo!()
+}
