8b4168092eb118af2b6045bcc2e975da65d5e412
kalan lee <kalanjlee@gmail.com>
2024-02-28 10:19:17 +1100

    Update

--- src/paste/functions.rs
+++ src/paste/functions.rs
@@ -18,7 +18,13 @@ pub fn copy_folder_contents_to(origin: &Path, destination: &Path) -> Result<Stri
     todo!()
 }
 
-pub fn move_item_to(origin: &Path, destination: &Path) -> Result<String, String> {
+pub fn move_file_to() {}
+
+pub fn move_file_contents_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    todo!()
+}
+
+pub fn move_folder_to(origin: &Path, destination: &Path) -> Result<String, String> {
     let name = origin
         .file_name()
         .ok_or("Unnamed copy directory")?
@@ -26,8 +32,85 @@ pub fn move_item_to(origin: &Path, destination: &Path) -> Result<String, String>
         .to_string();
 
     let new_des = destination.join(name);
-    if new_des.exists() {
-        println!("Item already exists, do you want to replace it? (y/n)");
+
+    todo!()
+}
+
+pub fn move_folder_contents_to(origin: &Path, destination: &Path) -> Result<String, String> {
+    if destination.exists() {
+        todo!()
+
+        //TODO
+        //Delete original directory
+    } else {
+        todo!()
+    }
+}
+
+enum EntryType {
+    File,
+    Folder,
+    Other,
+    None,
+}
+impl EntryType {
+    pub fn get(path: &Path) -> Self {
+        if path.exists() {
+            if path.is_file() {
+                EntryType::File
+            } else if path.is_dir() {
+                EntryType::Folder
+            } else {
+                EntryType::Other
+            }
+        } else {
+            EntryType::None
+        }
+    }
+}
+
+fn move_entry_ref(origin: &Path, destination: &Path) -> Result<(), String> {
+    let origin_type = EntryType::get(origin);
+    let destination_type = EntryType::get(destination);
+
+    match (origin_type, destination_type) {
+        // Skips because the entry no longer exists
+        (EntryType::None, _) => return Ok(()),
+        // The destination does not exist so can be cleanly moved
+        (_, EntryType::None) => match std::fs::rename(origin, destination) {
+            Ok(_) => (),
+            Err(_) => {
+                println!(
+                    "Unable to move \"{}\" to \"{}\", do you want to cancel? (y)",
+                    truncate_path_string(origin),
+                    truncate_path_string(destination)
+                );
+    
+                let mut input = String::new();
+                std::io::stdin()
+                    .read_line(&mut input)
+                    .expect("Failed to read user input");
+        
+                let trimmed_input = input.trim().to_lowercase();
+        
+                if trimmed_input != "y" && trimmed_input != "yes" {
+                    println!("Skipping...");
+                    return Err("cancelled".to_string());
+                }
+            },
+        },
+        // Copy the contents as it is a folder
+        (EntryType::Folder, EntryType::Folder) => todo!(),
+        // Already exists so will be replaced
+        (_, _) => todo!(),
+    }
+
+    if destination.exists() {
+        println!(
+            "Directory \"{}\" already exists, do you want to add the contents of \"{}\"? (y)",
+            truncate_path_string(destination),
+            truncate_path_string(origin)
+        );
 
         let mut input = String::new();
         std::io::stdin()
@@ -36,38 +119,19 @@ pub fn move_item_to(origin: &Path, destination: &Path) -> Result<String, String>
 
         let trimmed_input = input.trim().to_lowercase();
 
-        if trimmed_input == "y" || trimmed_input == "yes" {
-            return Ok("Cancelling...".to_string())
+        if trimmed_input != "y" && trimmed_input != "yes" {
+            println!("Skipping...");
+            return Ok(());
         }
-        
-        //DELETE FUNCTION
-        todo!()
-    }
 
-    rename_item(origin, &new_des)
-}
-
-pub fn move_file_contents_to(origin: &Path, destination: &Path) -> Result<String, String> {
-    if destination.exists() {
-        todo!()
-    }
-
-    rename_item(origin, destination)
-}
-
-pub fn move_folder_contents_to(origin: &Path, destination: &Path) -> Result<String, String> {
-    if destination.exists() {
-        todo!()
+        move_entry_contents(origin, destination)?;
     } else {
-        rename_item(origin, destination)
+        std::fs::rename(origin, destination).map_err(|_| "Invalid destination")?;
     }
+
+    Ok(())
 }
 
-fn rename_item(origin: &Path, destination: &Path) -> Result<String, String> {
-    std::fs::rename(origin, destination).map_err(|_| "Invalid destination")?;
-    Ok(format!(
-        "Moved item at \"{}\" to \"{}\"",
-        truncate_path_string(origin),
-        truncate_path_string(destination)
-    ))
+fn move_entry_contents(origin: &Path, destination: &Path) -> Result<(), String> {
+    todo!()
 }
